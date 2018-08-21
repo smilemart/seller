5fe8b16e5b64ee713ee6b8961c902f61f12fc561
vanism7908 <loveangel3326@gmail.com>
2018-08-21 18:21:18 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-    $host = "localhost";
-    $dbid = "kueleven";                    // 자신의 DB ID
-    $dbpass = "kueleven";                  // 자신의 DB Password
-    $dbname = "kueleven";                  // 해당 프로젝트에서 사용하는 DB명
-    $dbname_cu = "cu";
+    $host = "";
+    $dbid = "";                    // 자신의 DB ID
+    $dbpass = "";                  // 자신의 DB Password
+    $dbname = "";                  // 해당 프로젝트에서 사용하는 DB명
+    $dbname_cu = "";
 ?>
